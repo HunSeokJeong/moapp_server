84b8ceddb6371d730d9105203ebb9b14d25d6125
HunSeokJeong <44901486+HunSeokJeong@users.noreply.github.com>
2022-11-19 21:11:23 +0900

    [feat] admin창 추가

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Group, Room, History, Statics
 
+admin.site.register(Group)
+admin.site.register(Room)
+admin.site.register(History)
+admin.site.register(Statics)
 # Register your models here.
